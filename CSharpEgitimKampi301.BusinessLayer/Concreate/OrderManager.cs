e46291eff0fa254d50db2dec10f696a428732631
Osman Kuşoğlu <formatatalim@outlook.com>
2024-12-04 12:19:12 +0300

    Business Layer

---
+++ CSharpEgitimKampi301.BusinessLayer/Concreate/OrderManager.cs
@@ -0,0 +1,38 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Concreate
+{
+    public class OrderManager : IOrderService
+    {
+        public void TDelete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
